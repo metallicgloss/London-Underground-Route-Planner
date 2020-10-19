fa925aff9c627cd646f39269b0d0eff9bc981b46
William Phillips <metallicgloss@gmail.com>
2020-10-19 19:45:16 +0100

    Mess of a file commit to transfer to different device.

--- tfl_project/urls.py
+++ tfl_project/urls.py
@@ -1,10 +1,3 @@
-"""
-tfl_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-
-"""
 from django.contrib import admin
 from django.urls import path, include
 
