9a4914b963aa87d85d9e6a3b89811eb5cd065864
Daniel <59716545+It-is-Dan@users.noreply.github.com>
2020-10-22 15:55:55 +0100

    Adding user guide to the UI

--- tfl_route_planner/urls.py
+++ tfl_route_planner/urls.py
@@ -4,4 +4,5 @@ from tfl_route_planner import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('licences', views.licences, name='licences'),
+    path('userguide', views.userguide, name='userguide'),
 ]
