dbb3b2827d4ebd403fac599545b7eb27bb59fad2
Gabriel <gndb54@gmail.com>
2020-10-24 17:25:23 +0100

    Clears Whitespaces and missing Station Lines

--- Experiments_And_Prototypes/Prototypes/data_handler.py
+++ Experiments_And_Prototypes/Prototypes/data_handler.py
@@ -7,25 +7,58 @@ from station_manager_fixed import StationHandler
 
 
 if __name__ == "__main__":
-
     """Reading the Excel Sheet, and renaming the DataFrame Columns"""
     data = pd.read_excel("TFL_Underground_Data_Original.xlsx")
     data.columns = ["Line", "Origin", "Destination", "Time"]
     S = StationHandler()
 
-    print(data.iloc[50:150])
+
+    '''Sanitizing the missing Lines, making it comparing to the before and the after'''
+    for i in range(len(data.index)):
+        if pd.isna(data["Line"].iloc[i]):
+            print("No Line on index {}".format(i))
+            if data["Line"].iloc[i-1] == data["Line"].iloc[i+1]:
+                print("Changing index {} to {}".format(i,data["Line"].iloc[i+1]))
+                data.at[i,"Line"] = data["Line"].iloc[i+1]
+
+    '''Sanitizing the extra White Spaces'''
+    for i in range(len(data.index)):
+        if type(data.at[i, "Line"]) is str:
+            data.at[i, "Line"] = data["Line"].iloc[i].strip(" ")
+        if type(data.at[i, "Origin"]) is str:
+            data.at[i, "Origin"] = data["Origin"].iloc[i].strip(" ")
+        if type(data.at[i, "Destination"]) is str:
+            data.at[i, "Destination"] = data["Destination"].iloc[i].strip(" ")
+
 
     """Inserting all the stations in alphabetic order"""
     def insert_stations(dataframe, S):
-        for i in range(dataframe.size):
+        for i in range(len(dataframe.index)):
             try:
                 S.add_station_alphabetically(dataframe["Origin"].iloc[i])
             except Exception:
                 pass
         return S
 
+    """Inserting all the connections"""
+    def insert_connections(dataframe,S):
+        for i in range(dataframe.size):
+            origin = S.get_station_node_by_name(dataframe["Origin"].iloc[i])
+            destination = S.get_station_node_by_name(dataframe["Destination"].iloc[i])
+            line = dataframe["Line"].iloc[i]
+            time = int(dataframe["Time"].iloc[i])
+            try:
+                origin.add_station_connection(destination,time,line)
+            except Exception:
+                pass
+        return S
+
+
     S = insert_stations(data, S)
-    """This does return some duplicated stations, however, those are issues on the data sanitization"""
+    """This returns some duplicated stations, however, those are issues on the data sanitization with white spaces"""
     S.print_all_stations()
+    #S = insert_connections(data,S)
+    #S.print_all_stations()
+
 
 
